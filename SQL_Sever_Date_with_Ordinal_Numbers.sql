fcbd38d74bd61c146325a3432d8ec1643b68fa51
Leonardo <leonardo_batista@outlook.com>
2022-10-15 20:06:12 -0400

    Date with Ordinal Numbers

---
+++ SQL_Sever_Date_with_Ordinal_Numbers.sql
@@ -0,0 +1,23 @@
+
+SELECT
+    FORMAT(SOH.[OrderDate],'d'
+        + IIF(DAY(SOH.[OrderDate]) IN (1,21,31),'''st'''
+        , IIF(DAY(SOH.[OrderDate]) IN (2,22),'''nd'''
+        , IIF(DAY(SOH.[OrderDate]) IN (3,23),'''rd''','''th''')))
+        + ' MMMM yyyy') AS OrderDate
+    , SOH.[SalesOrderNumber]
+    , SOH.[AccountNumber]
+    , [OrderQty]
+    , PRD.[ProductID]
+    , PRD.[Name]
+    , PRD.[ProductNumber]
+    , [UnitPrice]
+    , [UnitPriceDiscount]
+    , [LineTotal]
+  FROM [AdventureWorks2019].[Sales].[SalesOrderDetail] AS SOD WITH(NOLOCK)
+    INNER JOIN [AdventureWorks2019].[Production].[Product] AS PRD WITH(NOLOCK)
+        ON (SOD.[ProductID] = PRD.[ProductID])
+    INNER JOIN [AdventureWorks2019].[Sales].[SalesOrderHeader] AS SOH WITH(NOLOCK)
+        ON (SOD.[SalesOrderID] = SOH.[SalesOrderID])
+  WHERE FORMAT(SOH.[OrderDate],'yyyy-MM-dd') >= '2011-06-01'
+  AND FORMAT(SOH.[OrderDate],'yyyy-MM-dd') <= '2011-06-30'
